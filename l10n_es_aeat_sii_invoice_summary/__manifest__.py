e9eb5354e69223208f29d3f1dc7afd1d86aacd94
cesar <cesarportas@alialabs.com>
2025-09-03 15:26:10 +0200

    [MIG] l10n_es_aeat_sii_invoice_summary: Migration to 12.0

--- l10n_es_aeat_sii_invoice_summary/__manifest__.py
+++ l10n_es_aeat_sii_invoice_summary/__manifest__.py
@@ -3,13 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Envio de factura simplificada resumen TPV a SII",
-    "version": "17.0.1.0.0",
-    "depends": ["l10n_es_aeat_sii_oca"],
+    "version": "12.0.1.0.0",
+    "depends": ["l10n_es_aeat_sii"],
     "category": "Accounting & Finance",
     "author": "FactorLibre, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-spain",
-    "data": ["views/account_move_view.xml"],
+    "data": ["views/account_invoice_view.xml"],
     "installable": True,
     "application": False,
 }
